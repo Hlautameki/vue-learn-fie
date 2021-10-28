8041d9b1098aaa2435d8e2ba00f5caf6ab474c8f
Mateusz Mistecki <mateusz.mistecki@gmail.com>
2021-10-28 22:43:39 +0200

    Added bulma

--- src/components/Hello/Hello.vue
+++ src/components/Hello/Hello.vue
@@ -1,9 +1,27 @@
 ﻿<template>
-  <div>Hello</div>
+  <div class="box is-flex is-flex-direction-column is-align-content-center is-align-self-center is-align-items-center">
+    <!-- <div style="display:flex; justify-content:center"> -->
+    <button class="button is-primary has-background-black">{{buttonContent}}</button>
+    <button class="button has-background-warning">{{getButtonContent()}}</button>
+    <p><a :href="wykopLink" target="_blank">Wykop</a></p>
+  </div>
 </template>
 
 <script>
+import '../../styl/main.styl'
+
 export default {
-  name: 'Hello'
+  name: 'Hello',
+  data() {
+    return {
+      buttonContent: 'Press Me',
+      wykopLink: 'https://www.wykop.pl'
+    }
+  },
+  methods: {
+    getButtonContent() {
+      return 'Press me'
+    }
+  }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './styl/main.styl'
+import 'bulma/css/bulma.css'
 
 createApp(App).mount('#app')
