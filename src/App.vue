0b09c435fafec3713fe27091d8391e4e6d8816e1
Mateusz Mistecki <mateusz.mistecki@gmail.com>
2021-10-30 13:22:56 +0200

    Added Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
 <template>
-  <Hello></Hello>
+  <ninja-coffee></ninja-coffee>
 </template>
 
 <script>
 import Hello from './components/Hello/Hello.vue'
+import NinjaCoffee from './components/NinjaCoffee/NinjaCoffee.vue'
 
 export default {
   name: 'App',
   components: {
-    Hello
+    Hello,
+    NinjaCoffee
   }
 }
 </script>
 
+
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+// #app
+//   font-family Avenir, Helvetica, Arial, sans-serif
+//   -webkit-font-smoothing antialiased
+//   -moz-osx-font-smoothing grayscale
+//   text-align center
+//   color #2c3e50
+//   margin-top 60px
 </style>
---
+++ src/components/NinjaCoffee/NinjaCoffee.vue
@@ -0,0 +1,59 @@
+﻿<template>
+  <!-- <p class="is-size-1 has-text-centered">Ninja Coffee</p>
+  <p class="is-size-7 has-text-right">Ninja Coffee</p>
+  <p class="has-text-weight-bold">Ninja Coffee</p>
+  <p class="is-italic">Ninja Coffee</p>
+  <p class="is-uppercase has-text-primary">Ninja Coffee</p>
+  <p class="title">Title</p>
+
+  <p class="py-1 my-4">Hello Bulma</p>
+  <p class="py-6">Hello Bulma</p>
+  <p class="px-1 mx-5">Hello Bulma</p>
+  <p class="py-1">Hello Bulma</p>
+
+  <section class="section">
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, obcaecati molestiae! Molestias modi sit facilis reprehenderit obcaecati. Numquam impedit at mollitia, consequatur facere dolorum molestiae rem deleniti quidem maiores odit.</p>
+  </section>
+
+  <div class="section">
+    <div class="container">
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, obcaecati molestiae! Molestias modi sit facilis reprehenderit obcaecati. Numquam impedit at mollitia, consequatur facere dolorum molestiae rem deleniti quidem maiores odit.</p>
+    </div>
+  </div> -->
+  <nav class="navbar has-shadow is-white">
+    <div class="navbar-brand">
+      <a class="navbar-item">
+        <img src="../../assets/logo2.png" alt="site logo" style="max-height: 70px" class="py-2 px-2">
+      </a>
+      <a class="navbar-burger" @click="setNavbarMenuActive">
+        <span></span>
+        <span></span>
+        <span></span>
+      </a>
+    </div>
+
+    <div class="navbar-menu" v-bind:class="{ 'is-active': isNavbarMenuActive}" id="nav-links">
+      <div class="navbar-end">
+        <a class="navbar-item">My Account</a>
+        <a class="navbar-item">Shopping Cart(0)</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NinjaCoffee',
+  data() {
+    return {
+      isNavbarMenuActive: false
+    }
+  },
+  methods: {
+    setNavbarMenuActive() {
+      this.isNavbarMenuActive = !this.isNavbarMenuActive
+    }
+  }
+
+}
+</script>
